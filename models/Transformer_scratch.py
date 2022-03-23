18cbaafc80410ddeedd18000e4b9c2013e979443
nataliablasco00 <natalia.blascoa@e-campus.uab.cat>
2022-03-23 13:54:32 +0100

    Added Transformer module

--- models/Transformer_scratch.py
+++ models/Transformer_scratch.py
@@ -10,6 +10,7 @@ on my youtube channel!
 
 import torch
 import torch.nn as nn
+import numpy as np
 
 from torch.autograd import Variable
 import torch.nn.functional as F
@@ -34,7 +35,6 @@ class SelfAttention(nn.Module):
     def forward(self, values, keys, query, mask):
         # Get number of training examples
         N = query.shape[0]
-
         value_len, key_len, query_len = values.shape[1], keys.shape[1], query.shape[1]
 
         # Split the embedding into self.heads different pieces
@@ -97,7 +97,6 @@ class TransformerBlock(nn.Module):
 
     def forward(self, value, key, query, mask):
         attention = self.attention(value, key, query, mask)
-
         # Add skip connection, run through normalization and finally dropout
         x = self.dropout(self.norm1(attention + query))
         forward = self.feed_forward(x)
@@ -122,7 +121,7 @@ class Encoder(nn.Module):
         self.embed_size = embed_size
         self.device = device
         self.word_embedding = nn.Embedding(src_vocab_size, embed_size)
-        self.position_embedding = nn.Embedding(max_length, embed_size)
+        self.position_embedding = nn.Parameter(torch.randn(1, embed_size)) #nn.Embedding(max_length, embed_size)
 
         self.layers = nn.ModuleList(
             [
@@ -138,10 +137,10 @@ class Encoder(nn.Module):
 
         self.dropout = nn.Dropout(dropout)
 
-    def forward(self, out, mask):
-        # N, seq_length = x.shape[1], x.shape[0]
-        # positions = torch.arange(0, seq_length).expand(N, seq_length).to(self.device)
-        # out = self.dropout((self.word_embedding(x) + self.position_embedding(positions))) # TODO: try x + self.position_embedding
+    def forward(self, x, mask):
+        N, seq_length = x.shape[1], x.shape[0]
+        positions = torch.arange(0, seq_length).expand(N, seq_length).to(self.device)
+        out = self.dropout(x + self.position_embedding) # TODO: try x + self.position_embedding
 
         # In the Encoder the query, key, value are all the same, it's in the
         # decoder this will change. This might look a bit odd in this case.
@@ -251,35 +250,111 @@ class Transformer(nn.Module):
         self.trg_pad_idx = trg_pad_idx
         self.device = device
         self.max_length = max_length
+        self.src_vocab_size = src_vocab_size
+        self.trg_vocab_size = trg_vocab_size
 
-        # self.out = nn.Linear(embed_size, trg_vocab_size)
+        self.out = nn.Linear(embed_size, trg_vocab_size)
 
     def make_src_mask(self, src):
-        src_mask = (src != self.src_pad_idx).unsqueeze(1)
+        src_mask = (src == self.src_pad_idx).unsqueeze(1)
         # (N, 1, 1, src_len)
-        return src_mask.to(self.device)
+        return None # src_mask.to(self.device)
 
     def make_trg_mask(self, trg):
         N, trg_len = trg.shape
-        trg_mask = torch.tril(torch.ones((trg_len, trg_len))).expand(
+        """trg_mask = torch.tril(torch.ones((trg_len, trg_len))).expand(
             N, 1, trg_len, trg_len
-        )
+        )"""
+        #return trg_mask.to(self.device)
 
-        return trg_mask.to(self.device)
+        nopeak_mask = torch.triu(torch.ones((1, trg_len, trg_len)), diagonal=1)
+        nopeak_mask = Variable(nopeak_mask == 0)
+        #print((trg != 0).unsqueeze(1).to(self.device) & nopeak_mask.to(self.device))
+        #print(" -->", ((trg != 0).unsqueeze(1).to(self.device) & nopeak_mask.to(self.device)).shape)
+        return ((trg != 0).unsqueeze(1).to(self.device) & nopeak_mask.to(self.device)).unsqueeze(1)
+
+    def make_pad_mask(self, size):
+        mask = torch.tril(torch.ones(size, size) == 1)  # Lower triangular matrix
+        mask = mask.float()
+        mask = mask.masked_fill(mask == 0, float('-inf'))  # Convert zeros to -inf
+        mask = mask.masked_fill(mask == 1, float(0.0))  # Convert ones to 0
+        pad_mask = torch.tril(torch.zeros((size, size))).expand(32, 1, size, size)
+        return pad_mask.to(self.device)  # pad_mask.to
+
+    def predict(self, src, max_length=20, SOS_token=0, EOS_token=3):
+        """
+        Method from "A detailed guide to Pytorch's nn.Transformer() module.", by
+        Daniel Melchor: https://medium.com/@danielmelchor/a-detailed-guide-to-pytorchs-nn-transformer-module-c80afbc9ffb1
+        """
+        """self.eval()
+        final_out = []
+        for i in range(src.shape[0]):
+            src_aux = src[i, :, :]
+            src_mask = self.make_src_mask(src_aux)
+            enc_src = self.encoder(src_aux, src_mask)
+
+            y_input = torch.tensor([[SOS_token]], dtype=torch.long, device=self.device)
+
+            for _ in range(max_length):
+                # Get source mask
+                trg_mask = self.make_pad_mask(y_input.size(1)).to(self.device)
+
+                pred = self.decoder(y_input, enc_src, src_mask, trg_mask)
+
+                next_item = pred.topk(1)[1].view(-1)[-1].item()  # num with highest probability
+                next_item = torch.tensor([[next_item]], device=device)
+
+                # Concatenate previous input with predicted best word
+                y_input = torch.cat((y_input, next_item), dim=1)
+
+                # Stop if model predicts end of sentence
+                if next_item.view(-1).item() == EOS_token:
+                    break
+
+            final_out.append(y_input.view(-1).tolist())
+        return final_out"""
+        self.eval()
+        src = torch.transpose(src, 0, 1)
+        src_mask = self.make_src_mask(src)
+        enc_src = self.encoder(src, src_mask)
+        outputs = torch.zeros(src.shape[0], max_length)
+        outputs[:, 0] = 1
+        for i in range(1, max_length):
+            trg_mask = torch.triu(torch.ones((1, i, i)), diagonal=1)
+            trg_mask = Variable(trg_mask == 0).cuda()
+            out = self.decoder(outputs[:,:i].int().cuda(),
+                                          enc_src, src_mask, trg_mask)
+            out = F.softmax(out, dim=-1)
+            val, ix = out[:, -1].data.topk(1)
+            outputs[:, i] = torch.squeeze(ix)
+            #if ix[0][0] == 3:
+            #    break
+
+        return out
 
     def forward(self, src, trg):
-        src = torch.transpose(src, 0, 1) # (N, 1, dim_embedding)
         if trg is None:
-            trg = Variable(torch.LongTensor(
-                [self.trg_pad_idx] * src.shape[0]*self.max_length)).view(src.shape[0], self.max_length).to(self.device)
-            trg[:, 0] = 1
-        src_mask = self.make_src_mask(src)
+            #trg = Variable(torch.LongTensor(
+            #    [self.trg_pad_idx] * src.shape[0]*self.max_length)).view(src.shape[0], self.max_length).to(self.device)
+            #trg[:, 0] = 1
+            #trg_mask = self.make_pad_mask(self.max_length, src.shape[0])
+            return self.predict(src)
+
+        src = torch.transpose(src, 0, 1) # (N, 1, dim_embedding)
+
+
         trg_mask = self.make_trg_mask(trg)
+
+        #print(" trg -->", trg)
+        src_mask = self.make_src_mask(src)
+        #print(" mask -->", trg_mask)
         enc_src = self.encoder(src, src_mask)
         out = self.decoder(trg, enc_src, src_mask, trg_mask)
 
-        #out = F.log_softmax(self.out(enc_src), dim=-1)
+        # out = F.log_softmax(self.out(enc_src), dim=-1)
+
         return out
+
 
 
 
